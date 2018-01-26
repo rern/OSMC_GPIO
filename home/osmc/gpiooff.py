3523785db127b927786deb4d851e29a675ed63cb
rern <rernrern@gmail.com>
2018-01-26 15:30:18 +0700

    Update gpiooff.py

--- home/osmc/gpiooff.py
+++ home/osmc/gpiooff.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from gpio import *
+import gpio
 import os
 import sys
 
@@ -11,15 +11,15 @@ else:
 	import time
 
 	if off1 != 0:
-		GPIO.output(off1, 1)
+		GPIO.output(off1, OFF)
 	if off2 != 0:
 		time.sleep(offd1)
-		GPIO.output(off2, 1)
+		GPIO.output(off2, OFF)
 	if off3 != 0:
 		time.sleep(offd2)
-		GPIO.output(off3, 1)
+		GPIO.output(off3, OFF)
 	if off4 != 0:
 		time.sleep(offd3)
-		GPIO.output(off4, 1)
+		GPIO.output(off4, OFF)
 	
 	os.system('/usr/bin/sudo /usr/bin/pkill -9 gpiotimer.py &')
