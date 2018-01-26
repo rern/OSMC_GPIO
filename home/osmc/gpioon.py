b38178a87ded274a0f218657d2957914f777fbb6
rern <rernrern@gmail.com>
2018-01-26 15:31:18 +0700

    Update gpioon.py

--- home/osmc/gpioon.py
+++ home/osmc/gpioon.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from gpio import *
+import gpio
 import os
 
 if GPIO.input(onx[1]) == 0:
@@ -8,15 +8,15 @@ else:
 	import time
 
 	if on1 != 0:
-		GPIO.output(on1, 0)
+		GPIO.output(on1, ON)
 	if on2 != 0:
 		time.sleep(ond1)
-		GPIO.output(on2, 0)
+		GPIO.output(on2, ON)
 	if on3 != 0:
 		time.sleep(ond2)
-		GPIO.output(on3, 0)
+		GPIO.output(on3, ON)
 	if on4 != 0:
 		time.sleep(ond3)
-		GPIO.output(on4, 0)
+		GPIO.output(on4, ON)
 	
 	os.system('/usr/bin/sudo /home/osmc/gpiotimer.py &')
