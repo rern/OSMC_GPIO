183baf9ba2a850e8ac00ac2478ca3662304fc882
rern <rernrern@gmail.com>
2018-02-08 15:48:28 +0700

    Update gpiooff.py

--- home/osmc/gpiooff.py
+++ home/osmc/gpiooff.py
@@ -3,18 +3,17 @@ from gpio import *
 
 if GPIO.input( offx[ 1 ] ) == OFF and len( sys.argv ) == 1: # bypass on shutdown/reboot (with any argument)
 	os.system( '/usr/bin/xbmc-send -a "Notification(GPIO,Already OFF)"' )
-	exit()
+else:
+	if off1 != 0:
+		GPIO.output( off1, OFF )
+	if off2 != 0:
+		time.sleep( offd1 )
+		GPIO.output( off2, OFF )
+	if off3 != 0:
+		time.sleep( offd2 )
+		GPIO.output( off3, OFF )
+	if off4 != 0:
+		time.sleep( offd3 )
+		GPIO.output( off4, OFF )
 
-if off1 != 0:
-	GPIO.output( off1, OFF )
-if off2 != 0:
-	time.sleep( offd1 )
-	GPIO.output( off2, OFF )
-if off3 != 0:
-	time.sleep( offd2 )
-	GPIO.output( off3, OFF )
-if off4 != 0:
-	time.sleep( offd3 )
-	GPIO.output( off4, OFF )
-
-os.system( '/usr/bin/sudo /usr/bin/pkill -9 gpiotimer.py &' )
+	os.system( '/usr/bin/sudo /usr/bin/pkill -9 gpiotimer.py &' )
