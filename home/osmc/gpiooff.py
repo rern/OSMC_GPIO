e48c9287002e17b29e65fa819c0f2e682b19ad8a
rern <rernrern@gmail.com>
2018-02-08 09:07:14 +0700

    Update gpiooff.py

--- home/osmc/gpiooff.py
+++ home/osmc/gpiooff.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python
-import gpio
-import os
-import sys
+from gpio import *
 
-if GPIO.input(offx[1]) == 1:
-	if len(sys.argv) == 1: # bypass on shutdown/reboot (with any argument) 
-		os.system('/usr/bin/xbmc-send -a "Notification(GPIO,Already OFF)"')
+if GPIO.input( offx[ 1 ]) == 1 and len( sys.argv ) == 1: # bypass on shutdown/reboot (with any argument)
+	os.system( '/usr/bin/xbmc-send -a "Notification(GPIO,Already OFF)"' )
+	exit()
 
-else:
-	import time
+if off1 != 0:
+	GPIO.output( off1, OFF )
+if off2 != 0:
+	time.sleep( offd1 )
+	GPIO.output( off2, OFF )
+if off3 != 0:
+	time.sleep( offd2 )
+	GPIO.output( off3, OFF )
+if off4 != 0:
+	time.sleep( offd3 )
+	GPIO.output( off4, OFF )
 
-	if off1 != 0:
-		GPIO.output(off1, OFF)
-	if off2 != 0:
-		time.sleep(offd1)
-		GPIO.output(off2, OFF)
-	if off3 != 0:
-		time.sleep(offd2)
-		GPIO.output(off3, OFF)
-	if off4 != 0:
-		time.sleep(offd3)
-		GPIO.output(off4, OFF)
-	
-	os.system('/usr/bin/sudo /usr/bin/pkill -9 gpiotimer.py &')
+os.system( '/usr/bin/sudo /usr/bin/pkill -9 gpiotimer.py &' )
