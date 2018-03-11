479faf00431a1a4398fd13165f9aac6ba6979ead
rern <rernrern@gmail.com>
2018-03-11 12:24:12 +0700

    Update soundusb.py

--- home/osmc/soundusb.py
+++ home/osmc/soundusb.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 import sys
 import os
-import requests
 import xml.dom.minidom as minidom
 
 if len( sys.argv ) > 1: # usb 'remove'
@@ -17,8 +16,9 @@ path = '/sys'+ stdout.replace( 'controlC1\n', 'id' ) # path of id file
 id = os.popen( 'cat '+ path ).read().replace( '\n', '' )
 
 # "ALSA:@:CARD='+ id +',DEV=0" must be exactly, no spaces, as in /home/osmc/.kodi/userdata/guisettings.xml
-payload = '{"jsonrpc":"2.0", "method":"Settings.SetSettingValue", "params":{"setting":"audiooutput.audiodevice", "value":"ALSA:@:CARD='+ id +',DEV=0"}, "id":1}'
-# request.post(url, data = {'key':'value'}, headers = {'key':'value'})
-requests.post( 'http://localhost:'+ port +'/jsonrpc', data=payload, headers={'content-type': 'application/json'} )
+data = { "jsonrpc": "2.0", "method": "Settings.SetSettingValue", "params": { "setting": "audiooutput.audiodevice", "value": "ALSA:@:CARD='+ id +', DEV=0" }, "id": 1 }
 
-os.system( '/usr/bin/xbmc-send -a "Notification(AUDIO OUTPUT,Switched to USB DAC)"' )
+req = urllib2.Request( url, json.dumps( data ), headers = headerdata )
+response = urllib2.urlopen( req )
+
+os.system( '/usr/bin/xbmc-send -a "Notification( AUDIO OUTPUT, Switched to USB DAC )"' )
